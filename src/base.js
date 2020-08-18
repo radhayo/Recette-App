01f4b91f11ab6ec119ca46f11f0dcac797e196ab
kira <radhamanthes>
2020-08-18 16:43:35 +0200

    64 Connecter l'app avec Firebase

--- src/base.js
+++ src/base.js
@@ -3,9 +3,9 @@ import firebase from 'firebase/app'
 import 'firebase/database'
 
 const firebaseApp = firebase.initializeApp({
-  apiKey: 'xxx',
-  authDomain: 'xxx',
-  databaseURL: 'xxx'
+  apiKey: "AIzaSyDX7LQqlHLXOhEYitOcoZPm5R0D9x0xjmc",
+    authDomain: "recettes-app-ce7fe.firebaseapp.com",
+    databaseURL: "https://recettes-app-ce7fe.firebaseio.com",
 })
 
 const base = Rebase.createClass(firebaseApp.database())
