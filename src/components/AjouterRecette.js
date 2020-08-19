9a0f2dd6ebeda8dcd97fded46e51cba19bcaa2e5
kira <radhamanthes>
2020-08-19 22:31:35 +0200

    67 Modifier le state via le formulaire

--- src/components/AjouterRecette.js
+++ src/components/AjouterRecette.js
@@ -12,17 +12,27 @@ class AjouterRecette extends Component {
         const { name, value } = event.target
         this.setState({ [name]: value })
     }
-
+    handleSubmit = event => {
+        event.preventDefault()
+        const recette = {...this.state }
+        this.props.ajouterRecette(recette)
+        // Reset
+        Object.keys(recette).forEach(item => {
+            recette[item] = ''
+        })
+        this.setState({...recette })
+    }
 
 
     render() {
         return (
             <div className= "card">
-                < form className="admin-form ajouter-recette">
+                < form className="admin-form ajouter-recette" onSubmit={this.handleSubmit}>
                     <input value = {this.state.nom} onChange={this.handleChange} name="nom" type="text" placeholder='Nom de la recette'/>
                     <input value = {this.state.image} onChange={this.handleChange} name="image" type="text" placeholder={'Nom de l\'image'}/>
                     <textarea value = {this.state.ingredients} onChange={this.handleChange} name="ingredients" placeholder='Liste des ingrédients'  rows="3"/>
                     <textarea value = {this.state.instructions} onChange={this.handleChange} name="instructions" placeholder='Liste des instructions'  rows="15"/>
+                <button type='submit'> + Ajouter une recette </button>
                 </form>
             </div>
         );
