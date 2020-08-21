726e97542e9856c325c1290b261606d399a31aa0
kira <radhamanthes>
2020-08-21 16:45:14 +0200

    69 gérer le formulaire de modifications

--- src/components/AdminForm.js
+++ src/components/AdminForm.js
@@ -1,16 +1,28 @@
 import React from 'react';
 
-const AdminForm = () => {
+const AdminForm = ({
+    id: key,
+    majRecette,
+    recettes
+}) => {
+    const recette= recettes[key]
+
+    const handleChange = (event, key) => {
+        const { name, value } = event.target
+        const recette = recettes[key]
+        recette[name] = value 
+        majRecette(key,recette)
+    }
     return (
         <div className='card'>
             <form className='admin-form'>
-                <input type='text' name ='nom' placeholder='Nom de la recette'/>
+                <input value={recette.nom} onChange={e => handleChange(e,key)} type='text' name ='nom' placeholder='Nom de la recette'/>
             
-                <input type='text' name ='image' placeholder=" Adresse de l'image " />
+                <input value={recette.image} type='text' name ='image' placeholder=" Adresse de l'image " />
 
-                <textarea  name ='ingredients' rows='3' placeholder='Liste des ingrédients'/>
+                <textarea value={recette.ingredients} name ='ingredients' rows='3' placeholder='Liste des ingrédients'/>
 
-                <textarea  name ='instructions' rows='15' placeholder='Liste des instructions'/>
+                <textarea value={recette.instructions} name ='instructions' rows='15' placeholder='Liste des instructions'/>
             </form> 
             <button>Supprimer</button>  
         </div>
