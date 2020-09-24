9194ccb3fb04c842652e71a8b80be969b950eadf
radhayo <romain.cianfarani@hotmail.fr>
2020-09-24 19:50:12 +0200

    Updates

---
+++ precache-manifest.a3f09ec1d1cef339b68bb12ebc88e6e7.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = [
+  {
+    "revision": "815852b98cfd976880da9f88ecc5892e",
+    "url": "/Recette-App/static/media/epinard.815852b9.jpg"
+  },
+  {
+    "revision": "410efa3616755b52959a",
+    "url": "/Recette-App/static/css/main.b2de39e8.chunk.css"
+  },
+  {
+    "revision": "f65e562a3858aff61c45",
+    "url": "/Recette-App/static/js/1.f65e562a.chunk.js"
+  },
+  {
+    "revision": "636a5fc935ac9ddce164",
+    "url": "/Recette-App/static/js/runtime~main.636a5fc9.js"
+  },
+  {
+    "revision": "b9a86c50cc3fe786eb584dd4dc784caf",
+    "url": "/Recette-App/static/media/default.b9a86c50.jpeg"
+  },
+  {
+    "revision": "4200b6b96b6c171e52c4a97d34a1174e",
+    "url": "/Recette-App/static/media/chocolat.4200b6b9.jpeg"
+  },
+  {
+    "revision": "410efa3616755b52959a",
+    "url": "/Recette-App/static/js/main.410efa36.chunk.js"
+  },
+  {
+    "revision": "fca17319421519cbb9d73c85b4879566",
+    "url": "/Recette-App/static/media/hamburger.fca17319.jpeg"
+  },
+  {
+    "revision": "287d53ae5223bf74627baa68ecc5f5c1",
+    "url": "/Recette-App/static/media/pizza.287d53ae.jpeg"
+  },
+  {
+    "revision": "8324a6142c5fc9d0bdd0b453c162f9a1",
+    "url": "/Recette-App/static/media/saumon.8324a614.jpeg"
+  },
+  {
+    "revision": "96c2f7548bab68d96c489276dbc837e6",
+    "url": "/Recette-App/static/media/tartine.96c2f754.jpg"
+  },
+  {
+    "revision": "1febada741163c8e96c69916dae392de",
+    "url": "/Recette-App/index.html"
+  }
+];
